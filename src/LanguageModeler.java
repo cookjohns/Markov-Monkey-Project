1909a749f975796f77e25c807f4ac1dfbd30f9da
John Cook <johncook@Johns-Macbook-Pro.local>
2015-04-03 00:52:07 -0500

    Updated comments

--- src/LanguageModeler.java
+++ src/LanguageModeler.java
@@ -16,7 +16,12 @@ public class LanguageModeler {
    HashMap<String, String> table = new HashMap<String, String>(17);
    char[] ca;
 	
-    // create an order K model for text
+   /*
+    * Constructor
+    * Creates an order K model for text
+    * @param K takes in length
+    * @param text takes in filename
+    */
    public LanguageModeler(int K, File text) throws FileNotFoundException {
       length = K;
       words = fileToString(text);
@@ -25,7 +30,12 @@ public class LanguageModeler {
       fillTable();
    }
    
-    // create an order K model for text
+   /*
+    * Constructor
+    * create an order K model for text
+    * @param K takes in length
+    * @param text takes in actual text of seed
+    */
    public LanguageModeler(int K, String text) {
       length = K;
       words = text;
@@ -34,14 +44,19 @@ public class LanguageModeler {
       fillTable();
    }
    
-    // return the first K characters of the sample text
+   /*
+    * Return the first K characters of the sample text
+    * @return the first seed
+    */
    public String firstSeed() {
       String result = words.substring(0, length);
       return result;
    }
    
-    // return K consecutive characters from the sample text,
-    // selected uniformly at random
+   /* Return K consecutive characters from the sample text,
+    *  selected uniformly at random
+    * @return the random seed
+    */
    public String randomSeed() {
       Random random = new Random();
       int start = random.nextInt(words.length() - length);
@@ -49,9 +64,12 @@ public class LanguageModeler {
       return result;
    }
    
-    // return a character that follows seed in the sample text,
-    // selected according to the probability distribution of all
-    // characters that follow seed in the sample text
+   /* Return a character that follows seed in the sample text,
+    *  selected according to the probability distribution of all
+    *  characters that follow seed in the sample text
+    * @param seed takes in the seed
+    * @return result returns the new seed
+    */
    public char nextChar(String seed) {
       String resultStr = (String) table.get(seed);
    //      if (resultStr == null) {
@@ -64,7 +82,11 @@ public class LanguageModeler {
    }
 //   }
    
-   // reads file to string
+   /*
+    * Reads file to string
+    * @param input takes in the filename
+    * @return result returns the text of the file
+    */
    private String fileToString(File input) throws FileNotFoundException {
       BufferedReader br = new BufferedReader(new FileReader(input));
       Scanner scan = new Scanner(br);
@@ -76,7 +98,9 @@ public class LanguageModeler {
       return result;
    }
    
-   // fill hashtable with word combinations of k length
+   /*
+    * Fill hashtable with word combinations of k length
+    */
    public void fillTable() {
       int i = 0;
       while (i < (words.length() - length)) {
@@ -96,7 +120,10 @@ public class LanguageModeler {
       }
    }
    
-   // gets total length of text
+   /*
+    * Gets total length of text
+    * @return length of the text
+    */
    public int textLength() {
       return words.length();
    }
